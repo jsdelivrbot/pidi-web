1d61a473e1180777d69a9e860897959acaf9d648
Teja Kantamneni <teja.kantamneni@gmail.com>
2018-07-22 09:08:19 -0400

    added missing import and test db

--- src/containers/Navbar/AccountMenu.js
+++ src/containers/Navbar/AccountMenu.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
+import IconButton from 'material-ui/IconButton'
+
 import DownArrow from 'material-ui/svg-icons/hardware/keyboard-arrow-down'
 import Avatar from 'material-ui/Avatar'
 import defaultUserImage from 'static/User.png'
